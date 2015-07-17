8a8fd0a907b3105751894cbea9e8dd5fdcf40cf6
David Moody <moody.david@gmail.com>
2015-07-16 21:35:56 -0400

    added a bunch of stuff

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 
-
 class IdeaBoxApp < Sinatra::Base
-  get '/' do
-      erb:Index
-  end
+    configure :development do
+        register Sinatra::Reloader
+    end
+    #handles the 404 errors
+    not_found do
+        erb :error
+    end
+    
+    get '/' do
+        erb:Index
+    end
+    #post '/' do
+    #    "creating an idea!"
+    #end
 end
