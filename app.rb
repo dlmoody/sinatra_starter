0d4fa148e74f9277145aa2d76cc857e7bbe5e2b7
David Moody <moody.david@gmail.com>
2015-07-17 22:26:18 -0400

    added deleting and stuff

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
+require './idea'
+require 'yaml/store'
 
 class IdeaBoxApp < Sinatra::Base
+    set :method_override, true
+
     configure :development do
         register Sinatra::Reloader
     end
@@ -7,11 +11,38 @@ class IdeaBoxApp < Sinatra::Base
     not_found do
         erb :error
     end
-    
+
     get '/' do
-        erb:Index
+        erb :index, locals: { ideas: Idea.all }
     end
-    #post '/' do
-    #    "creating an idea!"
-    #end
+
+    post '/' do
+        idea = Idea.new( params['title'], params['description'] )
+        idea.save
+        redirect '/'
+        # params.inspect
+    end
+
+    delete '/:id' do |id|
+        Idea.delete(id.to_i)
+        redirect '/'
+    end
+
+    get '/:id/edit' do |id|
+      idea = Idea.find(id.to_i)
+      erb :edit, locals: { id: id, idea: idea}
+    end
+
+    put '/:id' do |id|
+      data = {
+        :title => params['title'],
+        :description => params['description']
+      }
+      Idea.update(id.to_i, data)
+      redirect '/'
+
+    end
+
+
+
 end
